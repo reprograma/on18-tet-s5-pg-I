123c9dbbbb9aa84113e5b03e272f46b932ce609f
karolineeM <karolinem.andrade@gmail.com>
2022-09-15 16:42:37 -0300

    Exercicios semanais para entrega até o 11

--- exercicios/para-sala/revisao/exercicio10.js
+++ exercicios/para-sala/revisao/exercicio10.js
@@ -1,6 +1,9 @@
 // Dado uma lista:
-const professoras = ["Melissa", "Jéssica", "Bruna", "Emanuelle"]
+const professoras = ["Melissa", "Jéssica", "Bruna", "Emanuelle"];
 
 /*Adicione mais um item na primeira posição do array.
 Item: "Ana"
 Em seguida, mostre no console.log()*/
+
+let novodado = professoras.unshift("Ana");
+console.log(professoras);
--- exercicios/para-sala/revisao/exercicio11.js
+++ exercicios/para-sala/revisao/exercicio11.js
@@ -1,2 +1,16 @@
 // 2) Retornar frutas com somente a primeira letra maiúscula
 const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
+
+const listaFrutas = frutas.map((fruta) => fruta[0].toUpperCase() + fruta.slice(1).toLowerCase());
+
+console.log(listaFrutas);
+revisao/exercicio10.js
+revisao/exercicio11.js
+revisao/exercicio2.js
+revisao/exercicio3.js
+revisao/exercicio4.js
+revisao/exercicio5.js
+revisao/exercicio6.js
+revisao/exercicio7.js
+revisao/exercicio8.js
+revisao/exercicio9.js
