88c21c4f9f2694e18a24eaf31e416246cb091bad
palomamendesr <palomamendesr@gmail.com>
2022-09-26 16:36:13 -0300

    Projeto Guiado S5 finalizado

---
+++ exercicios/projeto/database.js
@@ -0,0 +1,115 @@
+const livros = [{
+        titulo: "Livro de Filosofia da Computação",
+        categoria: "tecnologia",
+        paginas: 200,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "Biografia Conceição Evaristo",
+        categoria: "biografia",
+        paginas: 360,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "Crespusculo",
+        categoria: "ficcao",
+        paginas: 460,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "Um Lugar Bem Longe Daqui",
+        categoria: "ficcao",
+        paginas: 390,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "Prólogo, Ato, Epílogo",
+        categoria: "biografia",
+        paginas: 660,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "Código Limpo",
+        categoria: "tecnologia",
+        paginas: 420,
+        leu: false,
+        recomenda: false
+    },
+    {
+        titulo: "É Assim Que Acaba",
+        categoria: "romance",
+        paginas: 368,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "A Garota do Lago",
+        categoria: "suspense",
+        paginas: 296,
+        leu: false,
+        recomenda: false
+    },
+    {
+    titulo: "A Quarta Revolução industrial",
+        categoria: "tecnologia",
+        paginas: 194,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "Diário de Anne Frank",
+        categoria: "biografia",
+        paginas: 276,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "Duna",
+        categoria: "ficcao",
+        paginas: 843,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "As Melhores Histórias de Viagens no Tempo",
+        categoria: "ficcao",
+        paginas: 500,
+        leu: true,
+        recomenda: true
+    },
+    {
+        titulo: "Frida – A Biografia",
+        categoria: "biografia",
+        paginas: 661,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "Steve Jobs",
+        categoria: "tecnologia",
+        paginas: 624,
+        leu: false,
+        recomenda: false
+    },
+    {
+        titulo: "Até o verão terminar",
+        categoria: "romance",
+        paginas: 336,
+        leu: true,
+        recomenda: false
+    },
+    {
+        titulo: "A sombra do vento",
+        categoria: "suspense",
+        paginas: 464,
+        leu: false,
+        recomenda: false
+    }
+]
+
+module.exports = livros
