962d9e699c0e1b62592daa06e53611aef430810d
Nataliasalles1 <natalia.salles100@gmail.com>
2022-09-26 16:35:28 -0300

    projeto tentativa nº 1000

--- exercicios/projeto/getLivroOrdenado.js
+++ exercicios/projeto/getLivroOrdenado.js
@@ -2,9 +2,53 @@ const read = require('readline-sync');
 const livros = require('./database');
 
 
-const getLivroOrdenado = () => {
-    //implementar a lógica COM AS PERGUNTA
-}
+const read = require('readline-sync');
+const livros = require('./database');
 
+
+const getLivroOrdenado = () => {
+
+    const opcaoOrdenado = read.question(`Você deseja procurar o livro de qual modo? 
+    1- Páginas;
+    2- Recomendação;
+     `);
+
+    if (opcaoOrdenado === '1') {
+        livros.sort(function (ordenar, ordenarb) {
+            if (ordenar.paginas < ordenarb.paginas) {
+                return -1;
+            } else {
+                return true;
+            }
+        })
+
+        const getInfoLivros = livros.map((livro) => {
+            return {
+                titulo: livro.titulo,
+                paginas: livro.paginas
+            }
+        })
+        console.table(getInfoLivros)
+
+        if (opcaoOrdenado === 'N') {
+            console.log("Volte ao menu principal.")
+        }
+
+        const confirmarOrdenacao = read.question(`Escolha o livro de acordo com a tabela: `).toUpperCase();
+
+        const confirmarPaginas = read.question(`Tem certeza? S/N `).toUpperCase();
+
+        if (confirmarPaginas === 'S') {
+            const filtrarPorOrdemCrescente = livros.filter((livro) => livro.paginas === confirmarOrdenacao) 
+            console.table(filtrarPorOrdemCrescente)
+        } else {
+            console.log("Resposta inválida")
+        }
+
+        if (confirmarPaginas === 'N'){
+            console.log(getInfoLivros)
+        } 
+    }
+}
 
 module.exports = getLivroOrdenado
